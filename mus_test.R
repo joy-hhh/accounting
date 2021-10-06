c42f5be9fc52c589b47966eaf369a34d73126073
joy-hhh <joyhhh@outlook.kr>
2021-10-06 21:06:25 +0900

    mus sampling

--- mus_test.R
+++ mus_test.R
@@ -1,25 +1,36 @@
 library(tidyverse)
+library(writexl)
 
 je <- read_csv("https://raw.githubusercontent.com/joy-hhh/R_for_JE_test/main/je_utf.csv")
 pop <- je %>% 
     filter(ACCTCD  == '40401')
 
-
-pop <- pop %>% 
-    mutate(cum = cumsum(CR))
-
+pop <- pop %>% rename(amount = CR)
 
 interval <- 300000000
 nu <- pop %>% 
-    select(CR) %>% 
+    select(amount) %>% 
     sum()/interval + 1 
 nu <- round(nu)
+
+
     
 sampling_row <- seq(nu)    
 sampling_n <- seq(nu) * interval
 
+pop <- pop %>% 
+    mutate(cum = cumsum(amount))
 
 for (i in seq_along(sampling_n)) {
     sampling_row[i] <- which(pop$cum > sampling_n[i])[1]
 }
+
 length(na.omit(sampling_row))
+
+sampling <- pop %>% 
+    select(-cum) %>% 
+    slice(sampling_row)
+
+print(sampling, n = Inf)
+
+sampling %>% write_xlsx("sampling.xlsx")
