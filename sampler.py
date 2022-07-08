f2eba549613daa293cb9e019c7e6ea34887b50e2
joy-hhh <joyhhh@outlook.kr>
2022-07-08 13:49:13 +0900

    sampler xlsx

--- sampler.py
+++ sampler.py
@@ -40,18 +40,16 @@ def save_File():
         Test = [' ',
                 'Test of Details - [BP70 Section A-B]',
                 ' ',
-                ' ',
                 '회사명',
                 '작성일자',
                 '작성자',
                 '검토자',
                 ' ',
                 ' ',
-                ' ',
                 'Section A: 목적 ',
                 ' ',
                 '표본감사를 사용할 때 감사인의 목적은 추출된 표본이 모집단에 대해 결론을 도출하는 데 합리적인 근거를 제공하는 것이다. ',
-                '표본감사란 계정잔액이나 특정 거래 유형의 특성을 평가하기 위한 목적으로, 전체항목(100％)보다 적은 수의 항목에 대해서 감사절차를 적용하는 것이다.',
+                '표본감사란 계정잔액이나 특정 거래의 입증을 목적으로, 전체 항목보다 적은 수의 항목에 대해서 감사절차를 적용하는 것이다.',
                 '표본항목들은 표본이 모집단을 대표하는 방식으로 추출되어야 한다. 그러므로 모집단의 모든 항목들은 추출될 기회를 가져야 한다. ',
                 '감사인은 감사표본을 설계할 때 감사절차의 목적과 표본을 도출할 모집단의 특성을 고려하여야 한다. ',
                 '감사인은 감사표본을 설계할 때 달성할 특정 목적과 그러한 목적을 가장 잘 달성할 수 있는 감사절차의 조합을 고려해야 한다. ',
@@ -63,65 +61,94 @@ def save_File():
                 '(3) 테스트되는 경영자의 주장 (Assertion)  :    정확성 (A), 실재성 및 발생사실(E/O)',
                 ' ',
                 ' ',
-                ' ',
                 'Section B: 표본 설계 - 모집단과 표본',
                 ' ',
-                '(1) 모집단의 성격: ',
-                '(2) 모집단의 완전성 확인  : ',
+                '(1) 모집단의 성격 : ',
+                '(2) 모집단의 완전성 확인 방법 : ',
                 '(3) 표본단위의 정의  : ',
                 '(4) 전체 모집단이 추출 대상인가?  : ',
                 ' ',
                 ' ',
-                '표본 지표',
-                ' ',
-                '모집단 크기 : ' + str(sum(pop['amount'])),
-                '예상오류 : ' + str(int(entry_PM.get()) * float(e.get())),
+                '* 표본 지표',
+                '---------------------------------------------------------------------------------------------------------------------------- ',
+                '모집단 크기 : ' + str(format(sum(pop['amount']), ',d')),
+                '예상오류 : ' + str(float(entry_PM.get()) * float(e.get())),
                 '표본대상 항목들의 위험평가 : ' + str(combobox1.get()),
                 '통제에 의존하는 경우 : ' + str(combobox2.get()),
                 '실증적 분석적 검토 절차를 통해 기대수준의 확신을 얻었는가? : ' + str(combobox3.get()),
+                '---------------------------------------------------------------------------------------------------------------------------- ',
+                ' ',
                 ' ',
                 '추출 방법 (MUS or Random): ',
                 ' ',
                 ' ',
-                ' ',
+                '---------------------------------------------------------------------------------------------------------------------------- ',
                 '[Assurance factor]                                Planned Level of Assurance from Substantive Analytical Procedures',
                 "SignificantRisk    RelianceonControls        High   Moderate   Low   APNP ",
-                "Yes                     No                             1.1        1.6          2.8         3",
+                "Yes                     No                             1.1         1.6          2.8         3",
                 "No                      No                             0          0.5          1.7         1,9",   
-                "Yes                     Yes                            0          0.2          1.4         1.6",
-                "No                      Yes                            0          0            0.3         0.5",
+                "Yes                     Yes                             0          0.2          1.4         1.6",
+                "No                     Yes                             0          0            0.3         0.5",
+                '---------------------------------------------------------------------------------------------------------------------------- ',
                 ' ',  
                 ' ',
-                '표본크기 결정',
-                ' ',
+                '* 표본크기 결정',
+                '---------------------------------------------------------------------------------------------------------------------------- ',
                 '신뢰계수 (Assurance factor) : ' + str(AF), 
-                '추출된 표본의 갯수 : ' + str(len(sampling)),
+                '추출된 표본의 갯수 : '  + str(len(sampling)),
+                '---------------------------------------------------------------------------------------------------------------------------- ',
                 ' ',
                 ' ',
-                ' ',
-                ' ',
-                'Test에 대한 추가 기술 >> ']
+                'Test에 대한 추가 기술 및 결론 : ']
         
         file_path = filedialog.askdirectory()
         
         excel_file = file_path + '/test_summary.xlsx'
         workbook = xlsxwriter.Workbook(excel_file)
         worksheet = workbook.add_worksheet('ToD')
+
         for row_num, value in enumerate(Test):
             worksheet.write(row_num, 1, value)
         cell_format1 = workbook.add_format()
         cell_format1.set_bottom(5)
-        for i in range(5, 9):
+        for i in range(4, 8):
             worksheet.write(f'C{i}', " ", cell_format1)
-        for i in [22, 23, 30, 31, 32, 33, 44, 64]:
+        for i in [20, 21, 27, 28, 29, 30, 43, 63]:
             worksheet.write(f'C{i}', " ", cell_format1)
-        worksheet.set_column(1, 2, 40)
+        worksheet.set_column(1, 3, 40)
+
+        cell_format2 = workbook.add_format()
+        cell_format2.set_bold()
+        cell_format2.set_font_color('blue')
+        worksheet.write('B43','추출 방법 (MUS or Random): ', cell_format2)
+        worksheet.write('B63','Test에 대한 추가 기술 및 결론 : ', cell_format2)
+
+        cell_format3 = workbook.add_format()
+        cell_format3.set_bg_color('green')
+
+        cell_format4 = workbook.add_format()
+        cell_format4.set_bg_color('#0099FF')
+
+
+        worksheet.write('B2', 'Test of Details - [BP70 Section A-B]', cell_format3)
+        worksheet.write('C2',' ',cell_format3)
+        worksheet.write('D2','표본추출절차서 ',cell_format3)
+
+        worksheet.write('B10', 'Section A: 목적 ', cell_format4)
+        worksheet.write('B25', 'Section B: 표본 설계 - 모집단과 표본 ', cell_format4)
+        for i in ['C10','D10','C25', 'D25']:
+            worksheet.write(i, " ", cell_format4)
+
+
+
         workbook.close()
+
+
 
         excel_writer = pd.ExcelWriter(file_path + '/test_sample.xlsx', engine='xlsxwriter')
         sampling.to_excel(excel_writer, sheet_name='test_sample')
         excel_writer.save()
-        msgbox.showinfo("Save complete", "test_sample.xlsx")
+        msgbox.showinfo("Save complete", "test_sample.xlsx, test_summary.xlsx")
     except Exception as err:
         msgbox.showerror("Error", err)
 
@@ -162,7 +189,7 @@ def rand():
         sampling = pop.loc[sampling_row]
         
         ## 추출된 샘플의 갯수
-        lbl11.configure(text="Sample Output Size : " + str(len(sampling)))
+        lbl12.configure(text="Sample Output Size : " + str(len(sampling)))
         
     except Exception as err:
         msgbox.showerror("Error", err)
@@ -216,7 +243,7 @@ def mus():
         sampling_row.sort()
 
         ## 추출된 샘플의 갯수
-        lbl11.configure(text="Sample Output Size : " + str(len(sampling_row) + len(high)))
+        lbl12.configure(text="Sample Output Size : " + str(len(sampling_row) + len(high)))
         
         ## 샘플링 객체 생성
         pop_remain = pop_remain.drop('cum', axis = 1)
@@ -241,7 +268,7 @@ label2.grid(row = 2, column = 0, sticky = W, padx =10)
 label3 = Label(win, text = "Substantive Analytical Procedures")
 label3.grid(row = 3, column = 0, sticky = W, padx =10)
 
-label4 = Label(win, text = "Expected Misstatement")
+label4 = Label(win, text = "Expected Misstatement Rate")
 label4.grid(row = 4, column = 0, sticky = W, padx =10)
 
 label5 = Label(win, text = "Tolerable Misstatement")
@@ -295,8 +322,11 @@ entry_PM.insert(0, "700000000")
 lbl10 = Label(win, text ="not uploaded yet")
 lbl10.grid(row=10, column=1, sticky = W,  pady = 20)
 
-lbl11 = Label(win, text ="Sampling output size")
-lbl11.grid(row=12, column=1, sticky = W,  pady = 20)
+lbl11 = Label(win, text ="Select amount column")
+lbl11.grid(row=11, column=1, sticky = W,  pady = 20)
+
+lbl12 = Label(win, text ="Sampling output size")
+lbl12.grid(row=12, column=1, sticky = W,  pady = 20)
 
 
 
